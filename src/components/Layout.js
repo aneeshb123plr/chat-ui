382a67aaf3e7279e2360bcb4b4ee7f8c4a3d35cf
aneeshb123plr <aneeshb123plr@gmail.com>
2019-12-03 11:59:31 +0530

    add layout

---
+++ src/components/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import '../App.css'
+export default class Layout extends Component {
+    render() {
+        return (
+            <div className="layout-container" >
+                <div></div>
+            </div>
+        )
+    }
+}
